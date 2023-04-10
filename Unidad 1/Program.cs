b10a8a905f15618c8f9f31b52d4e19409594ae98
JoseFabela <jfabela522@gmail.com>
2023-04-10 17:52:21 -0500

    Agregar archivos de proyecto.

---
+++ Unidad 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unidad_1
+{
+   
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Planificación
+            Console.WriteLine("Definir objetivos del proyecto");
+
+            // Diseño
+            Console.WriteLine("Crear diseño detallado");
+
+            // Implementación
+            Console.WriteLine("Escribir código");
+
+            // Pruebas
+            Console.WriteLine("Realizar pruebas");
+
+            // Mantenimiento
+            Console.WriteLine("Realizar actualizaciones");
+        }
+    }
+}
